9d8db92d473209d948dfac99a37ff2dd7d73c78b
David <macmini@DV-macmin.local>
2020-05-01 19:54:32 -0400

    fixed page imports for home and about pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-//package imports
 import { Route, BrowserRouter as Router } from "react-router-dom";
 import { Container } from "react-bootstrap";
-//component imports
-// import Container from '../src/components/Container';
 import Navbar from "../src/components/Navbar";
-// page imports
-// import Home from "./pages/Home";
-import Home from "./pages/Home/Home";
-import About from "./pages/About/About";
+import Home from "./pages/Home";
+import About from "./pages/About";
 import Webdev from "./pages/Webdev";
 import Marketing from "./pages/Marketing";
 import Creative from "./pages/Creative";
@@ -20,17 +14,13 @@ const App = () => (
   <Router>
     <Container>
     <Navbar />
-    
-   
       <Route exact path="/" component={Home} />
       <Route exact path="/about" component={About} />
       <Route exact path="/creative" component={Creative} />
       <Route exact path="/webDev" component={Webdev} />
       <Route exact path="/marketing" component={Marketing} />
       <Route exact path="/contact" component={Contact} />
-  
     </Container>
-
   </Router>
 );
 export default App;
