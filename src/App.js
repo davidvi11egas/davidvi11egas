1b9a6c3b0d0ff2cb54e1ef143344c99c8acee6f1
David <macmini@DV-macmin.local>
2020-05-01 23:34:40 -0400

    reimported bootstrap cdn

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { Route, BrowserRouter as Router } from "react-router-dom";
 import { Container } from "react-bootstrap";
