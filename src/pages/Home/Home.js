1947fa3a0da19b604a49345b882ce36c545cc9b9
David <macmini@DV-macmin.local>
2020-05-11 23:47:33 -0400

    saving

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,14 +7,17 @@ class Home extends Component {
   render() {
     return (
       <div className="pageContent">
-        <Row>
+        <Row id="row">
+    
           <img className="bioPic" src={require("../../images/davidFullBody.jpg")} alt="david_villegas_photo" />
           <div className= "tagLineBox">
             <p className="tagLineText"> david villegas </p>
             <p className="tagLineText"> web development</p>
             <p className="tagLineText"> marketing </p>
             <p className="tagLineText"> graphic design </p>
+            {/* <img className="bioPic" src={require("../../images/davidFullBody.jpg")} alt="david_villegas_photo" /> */}
           </div>
+        
         </Row>
       </div>
     );
