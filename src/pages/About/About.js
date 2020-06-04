c5d3e00a4764b6b807e2e9502d761b052c5de023
dvd-vllgs <davidvillegas627@gmail.com>
2020-06-04 16:17:04 -0400

    added new cover photo to home page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,25 +1,22 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './About.css';
-import data from './AboutData';
-import { Row, Col } from 'react-bootstrap';
+import {Row} from 'react-bootstrap';
 
 class About extends Component {
     render() {
         return (
             <div className="pageContent">
                 <header className="pageHeader"> About Me </header>
-                {/* <h3>{data.aboutData[1]}</h3> */}
                 <Row>
-                <div className="aboutMeText">
-                    <p>
-                    With an eye for captivating design, and a passion for innovation, David Villegas has over a decade of experience in digital marketing and creative production.
-                    In addition to digital marketing, David has talents spread across photography, cinematography, and musical production.
-                    Or, to put it frankly - David Villegas is a production house all in one. 
-                    
-                    </p>
-                </div>
-            </Row>
-           
+                    <img className="bioPic" src={require("../../images/davidFullBody.jpg")} alt="david_villegas_photo" />
+                    <div className="aboutMeText">
+                        <p>
+                            With an eye for captivating design, and a passion for innovation, David Villegas has over a decade of experience in digital marketing and creative production.
+                            In addition to digital marketing, David has talents spread across photography, cinematography, and musical production.
+                            Or, to put it frankly - David Villegas is a production house all in one.
+                        </p>
+                    </div>
+                </Row>
             </div>
         );
     }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -9,12 +9,11 @@ class Home extends Component {
       <div className="container pageContent">
         <div id="homePage">
           <Row className="row justify-content-md-center">
-            <img className="bioPic" src={require("../../images/davidFullBody.jpg")} alt="david_villegas_photo" />
             <div className="tagLineBox">
-              <p className="tagLineText" id="name"> david villegas </p>
-              <p className="tagLineText"> digital marketing</p>
-              <p className="tagLineText"> web developer </p>
-              <p className="tagLineText"> graphic designer </p>
+              <p className="tagLineText"> David Villegas </p>
+              <p className="tagLineText"> Digital Marketing </p>
+              <p className="tagLineText"> Web Development </p>
+              <p className="tagLineText"> graphic design </p>
             </div>
           </Row>
         </div>
