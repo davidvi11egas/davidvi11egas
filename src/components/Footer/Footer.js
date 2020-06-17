f21bd875ceab226ebb3c444640639303701766a0
David <dv_mbp@DV-mbp.local>
2020-06-16 22:49:32 -0400

    added logos to about me page

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,19 +1,30 @@
-// import React from 'react';
-// import "./Footer.css";
-// import {Card, Button, Image} from 'react-bootstrap'
+import React from 'react';
+import { Nav, NavItem, Button } from 'react-bootstrap';
+import linkedin from './icons/linkedin.png';
+import email from './icons/email.png';
 
-// const Footer = props => (
-  
-//     <div className="footer"> 
-     
-      
-  
-//     <Image src="../../images/parseroCafe.jpg" alt="linkedin"></Image>
+// icon download link: https://www.iconfinder.com/iconsets/social-media-outline-6
 
+function Footer() {
+    return (
+        <footer>
+            <Nav justified>
+                <NavItem>
+                    <Button variant="light">
+                        <img src={email} width='20px' alt='emailIcon' />
+                    </Button>
+                </NavItem>
+                <NavItem>
+                    <Button variant='light'>
+                        <img src={linkedin} width='20px' alt='linkedInIcon' />
+                    </Button>      
+                </NavItem>
+            </Nav>
+            <div className="text-center small copyright">
+                © David Villegas 2020
+            </div>
+        </footer>
+    );
+}
 
-//     </div>
-
-// )
-
-// export default Footer;
-
+export default Footer;
