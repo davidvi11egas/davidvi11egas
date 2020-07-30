7b265f89d7df99c201ec60daba3f36f2dc7decee
David <dv_mbp@DV-mbp.local>
2020-07-29 22:06:55 -0400

    added visitFlorida and upPup to webdev page

--- src/pages/Webdev/Webdev.js
+++ src/pages/Webdev/Webdev.js
@@ -1,23 +1,56 @@
 import React, { Component } from "react";
 import "./Webdev.css";
-
+import Navbar from  "../../components/Navbar";
+import visitFlorida from "../../images/visitFlorida.png";
+import upPup from "../../images/upPup.png";
 class Webdev extends Component {
   render() {
     return (
       <div className="container-fluid">
+      <Navbar/>
         <header className="pageHeader">WebDev</header>
-        <div className="card siteCard">
-          <div className="card-header">
-            <h1>CODE</h1>
-          </div>
-          <div className="card-body">
-            <img width="100" src="https://www.xmple.com/wallpaper/checkered-blue-squares-white-1920x1080-c2-00bfff-ffffff-l-240-a-60-f-2.svg" alt="sampleWebsite" />
-            <hr></hr>
+       
 
-
-          </div>
+          <div className="container text-center">
+          <div className="d-inline-flex">
+          <div className="row">
+          <div className="col">
+          <section id="lists">
+							<h3><a href="#collapseMenu5" data-toggle="collapse" aria-expanded="false"
+									aria-controls="collapseMenu5">Florida</a></h3>
+							<div className="collapse" id="collapseMenu5">
+								<div className="card card-body">
+									<p>
+                    Responsive HTML & CSS
+									</p>
+								</div>
+							</div>
+              <a href="https://davidvi11egas.github.io/visit_florida/" target="_blank">
+							<img src={visitFlorida} width="50%"
+								alt="Cocoa Beach Florida"/>
+                </a>
+						</section>
+            <section id="lists">
+							<h3><a href="#collapseMenu5" data-toggle="collapse" aria-expanded="false"
+									aria-controls="collapseMenu5">Up Pup</a></h3>
+							<div className="collapse" id="collapseMenu5">
+								<div className="card card-body">
+									<p>
+                    Responsive HTML & CSS
+									</p>
+								</div>
+							</div>
+              <a href="https://master.d35odzy1irc8uc.amplifyapp.com/" target="_blank">
+							<img src={upPup} width="50%"
+								alt="Cocoa Beach Florida"/>
+                </a>
+						</section>
+            </div>
+            </div>
+            </div>
         </div>
-      </div>
+        </div>
+  
     );
   }
 }
