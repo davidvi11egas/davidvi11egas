ee1027d36e13a7eead6bfc6d317d3f11de8485a4
David <dv_mbp@DV-mbp.local>
2021-05-03 15:27:32 -0400

    removed bio section from resume section

--- src/pages/About/About2.js
+++ src/pages/About/About2.js
@@ -11,44 +11,7 @@ class About2 extends Component {
             <Container>
                 {/* <Navbar /> */}
     
-                <Row>
-                    {/* <div id="resume"></div> */}
-                    <Col>
-                        <div className="pt-5">
-                            <h1>
-                                <strong>
-                                    David Villegas
-                                </strong>
-                            </h1>
-                        </div>
-                        <hr className="thickHR"/>
-                        <br/>
-                        <div className="">
-                        {/* <h5><strong>
-                            Digital Marketer . Web Developer . Creative Designer
-                            </strong>
-                            </h5> */}
-                           
-                            </div>
-                        <p id="personalBio">
-                            Born in Medellin Colombia, then raised in Central
-                            Florida, David Villegas pulls inspiration from the
-                            spirit and culture of the places that raised him.
-                            With extensive knowledge in a variety of industries
-                            such as web development, marketing, graphic design,
-                            and music production, David has the skills needed to
-                            take your any idea and make it into reality. With an
-                            eye for captivating design, love for technology, and
-                            a mind overflowing with creativity; David Villegas
-                            delivers innovation, quality and accuracy in web
-                            development, creative production, and digital
-                            marketing.Never stop learning, because life never
-                            stops teaching.
-                        </p>
-   
-                    </Col>
 
-                </Row>
                 <Skills />
                 <Experience />
                 <div>
